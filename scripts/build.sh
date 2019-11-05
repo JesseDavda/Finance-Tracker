11082eb96c901915a0ece8c2b43621b78d9aba80
JesseDavda <jessepilot@live.com>
2019-11-05 17:02:29 +0000

    Fix npm error

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,11 @@
 # Build the client
 cd client
 # Removes any old build files
-rm -rf ./build
 npm run build
 
 # Build the API
 cd ../api
 # Removes any old build files
-rm -rf ./dist
 npm run build
 
 # Back to root
