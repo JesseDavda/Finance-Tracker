49cb5ab3235471d3b2c098e8e8fb406cdafd130c
JesseDavda <jessepilot@live.com>
2019-11-11 18:26:06 +0000

    ..

--- api/src/routes/googleLoginHandler.js
+++ api/src/routes/googleLoginHandler.js
@@ -22,7 +22,7 @@ router.post('/googleOAuthTokenHandler', (req, res) => {
 
     axios.post(GOOGLE_CODE_EXCHANGE_URL, codeExchangeBody)
         .then(response => {
-            console.log(reponse.data);
+            console.log(response.data);
             storeUserInfo(response.data.access_token)
                 .then(data => {
                     console.log(data);
